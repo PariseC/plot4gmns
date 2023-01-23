34dd9e71edfe032f08b212cde3c89f1cca8487b8
PraiseC <63484736+PariseC@users.noreply.github.com>
2023-01-23 16:07:37 +0800

    update readme

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import plot4gmns as p4g
-mnet = p4g.generate_multi_network_from_csv(r'E:\CoderStudio\Py\2021-04-30-TestPackages\usingGrid2Demand\Berlin')
+import pandas as pd
+from keplergl import KeplerGl
 
-# cf = p4g.show_network_by_modes(mnet=mnet)
-# cf.show()
+mnet = p4g.generate_multi_network_from_csv(r'E:\CoderStudio\Py\2022-09-11-open_source_packages\Plot4GMNS\datasets\Berlin')
+
+cf = p4g.show_network_by_modes(mnet=mnet)
+cf.show()
 
 # cf = p4g.show_network_by_modes(mnet=mnet,modes=['bike'])
 # cf.show()
@@ -24,41 +27,8 @@ mnet = p4g.generate_multi_network_from_csv(r'E:\CoderStudio\Py\2021-04-30-TestPa
 
 # cf = p4g.show_network_by_link_lane_distribution(mnet=mnet)
 # cf.show()
-
+#
 # cf = p4g.show_network_by_link_capacity_distribution(mnet=mnet)
 # cf.show()
 # cf = p4g.show_network_by_link_free_speed_distribution(mnet=mnet)
-# cf.show()
-
-# cf = p4g.show_network_by_poi_types(mnet=mnet,poi_type=['public','industrial'])
-# cf.show()
-
-# cf = p4g.show_network_by_poi_attraction_distribution(mnet=mnet)
-# cf.show()
-
-# cf = p4g.show_network_by_poi_production_distribution(mnet=mnet)
-# cf.show()
-
-# cf = p4g.show_network_demand_matrix_heatmap(mnet)
-# cf.show()
-
-# cf = p4g.show_network_by_demand_OD(mnet=mnet)
-# cf.show()
-
-# mnet.node_loaded = False
-# cf = p4g.show_network_by_link_lane_distribution(mnet=mnet)
-# cf.show()
-
-# mnet.node_loaded = False
-# mnet.POI_loaded = False
-# cf = p4g.show_network_by_link_lane_distribution(mnet=mnet)
-# mnet.link_loaded = False
-# mnet.POI_loaded = True
-# cf = p4g.show_network_by_poi_attraction_distribution(mnet,fig_obj=cf)
-# cf.show()
-
-# mnet.style.node_style.size = 3
-# mnet.style.link_style.linecolor = 'green'
-# mnet.style.poi_style.facecolor = 'gray'
-# cf = p4g.show_network_by_modes(mnet=mnet)
 # cf.show()
